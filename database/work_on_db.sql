fc7adb7cdc19170b84898e71b7bee12038647fdf
Jessele Del Mundo <jesseledm@gmail.com>
2022-06-03 21:19:22 +0800

    Update work_on_db.sql
    update database

--- database/work_on_db.sql
+++ database/work_on_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2022 at 02:15 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Jun 03, 2022 at 01:18 PM
+-- Server version: 10.5.12-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,6 +42,35 @@ CREATE TABLE `project_list` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `project_list`
+--
+
+INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `proj_status`, `start_date`, `end_date`, `manager_id`, `chair_id`, `user_ids`, `user_type`, `date_created`) VALUES
+(1, 'Work-ON Project', 'Build Work-ON Project', 0, 0, '2022-06-03', '2022-06-04', 1, 2, '6,8,5,4,3,7', 3, '2022-06-03 12:20:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'WorkON', 'info@workon.com', '+6397701234567', '1111 New Build Avenue Drive Street Working Hub City, Philippines', 'work-on-logo.png');
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +93,46 @@ CREATE TABLE `task_list` (
   `notif_status` int(11) DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `task_list`
+--
+
+INSERT INTO `task_list` (`id`, `project_id`, `task`, `task_owner`, `description`, `admin_id`, `leader`, `status`, `task_startdate`, `active`, `admin_ok`, `chair_ok`, `notif_status`, `date_created`) VALUES
+(1, 1, 'Login Page', '3', 'Design Login UI', 1, 2, 1, NULL, 1, 0, 1, 1, '2022-06-03 12:22:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) DEFAULT 3 COMMENT '1=dean, 2=chair, 3=member',
+  `avatar` text NOT NULL DEFAULT 'no-image-available.png',
+  `task_status` int(11) NOT NULL DEFAULT 0 COMMENT '0=no-assigned,\r\n1=not-started,\r\n2=started,\r\n3=in-progress,\r\n4=in-review,\r\n5=completed',
+  `selected_id` int(11) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `task_status`, `selected_id`, `date_created`) VALUES
+(1, 'Administrator', ' ', 'admin@work-on.tech', '1ab1753615a7e91dc9cd1ed5d0d748cc', 1, 'no-image-available.png', 0, 0, '2022-05-21 16:43:24'),
+(2, 'Ash', 'Gray', 'ashgray@gmail.com', 'cd98e2b7dad295ed9207eb761f4eff48', 3, '1653446940_yanfei-genshin-impact.gif', 0, 0, '2022-05-21 16:47:35'),
+(3, 'Jenn', 'DM', 'jenndm@gmail.com', 'cd98e2b7dad295ed9207eb761f4eff48', 3, 'no-image-available.png', 0, 0, '2022-05-22 15:53:13'),
+(4, 'Clarence', 'Silva', 'cmsilva@gmail.com', '792f44ad367d31db88f8265c07f68095', 3, '1653500520_25247cb2dd5a22a4aa1a36603aedc151.jpg', 0, 0, '2022-05-25 17:42:13'),
+(5, 'Charlene', 'Malabanan', 'cmalabanan@work-on.com', '1653cce0dcb92232837c4c9b9bb10f63', 3, '1653500520_6e31399bb4da9736c94dfb8848569d73.jpg', 0, 0, '2022-05-25 17:42:57'),
+(6, 'Angela', 'Garcia', 'amgarcia@gmail.com', '779f5ee6da90efaaa98e7713bef54bb0', 3, '1653500580_main-qimg-5af86bc52047503194bec5597d96399b-lq.jpg', 0, 0, '2022-05-25 17:43:25'),
+(7, 'Jess', 'DM', 'jessdm@gmail.com', 'cd98e2b7dad295ed9207eb761f4eff48', 3, '1653837240_green-haired-anime-girl-1.jpg', 0, 0, '2022-05-29 15:14:19'),
+(8, 'Bryan', 'Hipolito', 'bii@gmail.com', '13521c72644704e973250bba9c1300a6', 3, 'no-image-available.png', 0, 0, '2022-05-30 05:19:37');
 
 -- --------------------------------------------------------
 
@@ -93,6 +162,13 @@ CREATE TABLE `user_productivity` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user_productivity`
+--
+
+INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `description`, `comment`, `user_id`, `file_name`, `file_type`, `file_size`, `file_path`, `status`, `notif_id`, `admin_id`, `leader`, `active`, `admin_ok`, `chair_ok`, `date_created`) VALUES
+(1, 1, 1, 'Finish Work-ON Login UI						', NULL, 3, 'images.png', 'png', 6434, 'assets/uploads/files/images.png', '1', 1, 0, 2, 0, 0, 0, '2022-06-03 13:11:33');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -103,9 +179,21 @@ ALTER TABLE `project_list`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `task_list`
 --
 ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,19 +210,31 @@ ALTER TABLE `user_productivity`
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_productivity`
 --
 ALTER TABLE `user_productivity`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
